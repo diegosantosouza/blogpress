5d62c66a5f9a40f1414670f0b00d3b3cc7ccc12a
diegosantosouza <diegosantos.s@hotmail.com>
2021-09-23 00:51:55 -0300

    fix
    create docker-compose  mysql:5.7 deploy

--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const connection = new Sequelize('blogpress', 'root', 'Y793jwLmge38xq', {
+const connection = new Sequelize('blogpress', 'root', '123456', {
     host: 'localhost',
     dialect: 'mysql',
     timezone: '-03:00'
