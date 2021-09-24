e1eebde226e797eff551f1866ac05894be4d0999
diegosantosouza <diegosantos.s@hotmail.com>
2021-09-24 20:35:59 -0300

    Fix
    Description User Model sync

--- user/Users.js
+++ user/Users.js
@@ -11,7 +11,8 @@ const User = connection.define('users', {
     }
 })
 
-//Cria a Tabela {{force}}: true =>recria a tablela toda vez, false=> se não existir ele cria senão não faz nada.
-//User.sync({force:true});
+//User.sync() - This creates the table if it doesn't exist (and does nothing if it already exists)
+//User.sync({ force: true }) - This creates the table, dropping it first if it already existed
+//User.sync({ alter: true }) - This checks what is the current state of the table in the database (which columns it has, what are their data types, etc), and then performs the necessary changes in the table to make it match the model.
 
 module.exports = User;
